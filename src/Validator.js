78c2a3d2e1dd1774ef382ae00ca446f4acf3d1ed
PavelRakhaev <rakhaevpavel@gmail.com>
2024-03-01 18:53:36 +0300

    added number validation

--- src/Validator.js
+++ src/Validator.js
@@ -1,8 +1,13 @@
+import NumberValidationSchema from "./NumberValidationSchema.js";
 import StringValidationSchema from "./StringValidationSchema.js";
 
 class Validator {
   string() {
     return new StringValidationSchema(this);
+  }
+
+  number() {
+    return new NumberValidationSchema(this);
   }
 }
 
