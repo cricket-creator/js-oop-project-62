1e9cebcb7f72e562118c62d390e97e46aa5e6677
PavelRakhaev <rakhaevpavel@gmail.com>
2024-03-01 17:47:05 +0300

    added string validation

---
+++ src/ValidationSchema.js
@@ -0,0 +1,23 @@
+class ValidationSchema {
+  constructor(validator) {
+    this.validator = validator;
+    this.validations = new Map();
+  }
+
+  addValidation(key, validationFn) {
+    this.validations.set(key, validationFn);
+  }
+
+  validate(value) {
+    const entries = [...this.validations];
+    return entries.every(([key, validationFn]) => {
+      const validationResult = validationFn(value);
+      if (!validationResult) {
+        console.log(`${key}: ${String(validationFn)} error!`);
+      }
+      return validationResult;
+    });
+  }
+}
+
+export default ValidationSchema;
