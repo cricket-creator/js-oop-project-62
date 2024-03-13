a60c2d88b0ee4a7a83a92344bbc45ee93c1c1058
PavelRakhaev <rakhaevpavel@gmail.com>
2024-03-13 14:38:48 +0300

    added object validation

---
+++ src/ObjectValidationSchema.js
@@ -0,0 +1,19 @@
+import ValidationSchema from "./ValidationSchema.js";
+
+class ObjectValidationSchema extends ValidationSchema {
+  shape(validationSchema) {
+    this.addValidation("shape", (object) => {
+      const keys = Object.keys(object);
+      return keys.every((key) => {
+        if (!Object.hasOwn(validationSchema, key)) {
+          return false;
+        }
+        const schema = validationSchema[key];
+        return schema ? schema.isValid(object[key]) : false;
+      });
+    });
+    return this;
+  }
+}
+
+export default ObjectValidationSchema;
